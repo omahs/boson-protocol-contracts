c9053d2178f1d83f21d9356b67c4e2c3fa04e120
zajck <klemen@impressieve.com>
2022-10-25 13:26:07 +0200

    dynamically get metaTx initArg

--- scripts/config/facet-upgrade.js
+++ scripts/config/facet-upgrade.js
@@ -1,3 +1,5 @@
+const { getStateModifyingFunctionsHashes } = require("../../scripts/util/diamond-utils.js");
+
 /**
  * Config file used to upgrade the facets
  *
@@ -9,18 +11,38 @@
  * - initArgs: if facet initializer expects arguments, provide it here. For no-arg initializers you don't have to specify anything.
  * - skipInit": list of facets for which you want to skip initialization call.
  */
-exports.Facets = {
-  addOrUpgrade: ["MetaTransactionsHandlerFacet"],
-  remove: [],
-  skipSelectors: { SellerHandlerFacet: [] },
-  initArgs: {
-    MetaTransactionsHandlerFacet: [
-      [
-        "0x1227dbbba1af7882df0c2f368ac78fb2c624a77dcfa783b3512a331d08541945",
-        "0x1843b3a936e72dc3423a7820b79df54578eb2321480ad2f0c6191b7a2c500174",
-        "0x4e534c9650f9ac7d5c03f8c48b0522522a613d6214bf7ba579412924ab0f9295",
-      ],
+async function getFacets() {
+  // metaTransactionsHandlerFacet initializer arguments.
+  const MetaTransactionsHandlerFacetInitArgs = await getStateModifyingFunctionsHashes(
+    [
+      "AccountHandlerFacet",
+      "SellerHandlerFacet",
+      "BuyerHandlerFacet",
+      "DisputeResolverHandlerFacet",
+      "AgentHandlerFacet",
+      "BundleHandlerFacet",
+      "DisputeHandlerFacet",
+      "ExchangeHandlerFacet",
+      "FundsHandlerFacet",
+      "GroupHandlerFacet",
+      "OfferHandlerFacet",
+      "OrchestrationHandlerFacet",
+      "TwinHandlerFacet",
+      "PauseHandlerFacet",
+      "MetaTransactionsHandlerFacet",
     ],
-  },
-  skipInit: [],
-};
+    ["executeMetaTransaction(address,string,bytes,uint256,bytes32,bytes32,uint8)"]
+  );
+
+  return {
+    addOrUpgrade: ["MetaTransactionsHandlerFacet"],
+    remove: [],
+    skipSelectors: { SellerHandlerFacet: [] },
+    initArgs: {
+      MetaTransactionsHandlerFacet: [MetaTransactionsHandlerFacetInitArgs],
+    },
+    skipInit: [],
+  };
+}
+
+exports.getFacets = getFacets;
